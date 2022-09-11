7113940de057e72c84156e7f5a1d027bb3e445a7
Ilya Volosovich <45130282+volosocheck@users.noreply.github.com>
2022-09-11 15:40:49 +0200

    Update prepare_data.py

--- pipeline/prepare_data.py
+++ pipeline/prepare_data.py
@@ -1,6 +1,7 @@
 import event_categories
+import pandas as pd
 
 def digitize_event_category_col(dt):
     dt_copy = dt.copy()
-    dt_copy['Событие'] = pd.Series(map(lambda n: EVENTS[n], list(dt_copy['Событие'])))
+    dt_copy['Событие'] = pd.Series(map(lambda n: event_categories.EVENTS[n], list(dt_copy['Событие'])))
     return dt_copy
