6882e88b0dfb4c380d9fb0235f9f30defa9e327e
Dinay Sharipov <dinaysm@gmail.com>
2022-09-11 21:58:42 +0600

    feat: data convertion function

--- pipeline/prepare_data.py
+++ pipeline/prepare_data.py
@@ -5,3 +5,10 @@ def digitize_event_category_col(dt):
     dt_copy = dt.copy()
     dt_copy['Событие'] = pd.Series(map(lambda n: event_categories.EVENTS[n], list(dt_copy['Событие'])))
     return dt_copy
+
+def convert_to_datetime(dt):
+    dt_copy = dt.copy()
+    dt_copy['Дата рождения'] = pd.to_datetime(dt_copy['Дата рождения'], format="%d/%m/%Y")
+    dt_copy['Дата события'] = pd.to_datetime(dt_copy['Дата события'], format="%d/%m/%Y")
+    return dt_copy
+    
